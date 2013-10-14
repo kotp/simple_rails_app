d1528050d986117b2094e1d65d75f6518fadbf07
Victor Goff <keeperotphones@gmail.com>
2013-10-14 01:47:02 -0500

    Fixed the 7 failing tests

--- spec/models/mobile_message_spec.rb
+++ spec/models/mobile_message_spec.rb
@@ -3,65 +3,60 @@ require 'spec_helper'
 describe MobileMessage do
 
   context 'validations' do
-
-    # validate presence
     it { should validate_presence_of(:message) }
+
     it { should validate_presence_of(:message_type) }
-
-    # allowed values
-    it { should allow_value('https://github.com/AlexZaytsev/simple_rails_app').for(:url) }
-    it { should allow_value('http://github.com/AlexZaytsev/simple_rails_app').for(:url) }
-    it { should allow_value('1.1').for(:platform_version) }
-    it { should allow_value('1.1.1').for(:platform_version) }
-    it { should allow_value('1.1.1').for(:app_version) }
-
-    # not allowed values
-    it { should_not allow_value('htt://github.com/AlexZaytsev/simple_rails_app').for(:url) }
-    it { should_not allow_value('').for(:app_version) }
-
-    # inclusions
-    it { should ensure_inclusion_of(:platform).in_array(%w(android ios)) }
-    #it {should ensure_inclusion_of(:platform_version).in_array(%w(1 2 3)).lambda {self.platform == "ios"} }
     it { should ensure_inclusion_of(:message_type).in_array(%w(webview alert)) }
 
+    it { should allow_value('https://google.com').for(:url) }
+    it { should_not allow_value('htt://google.com').for(:url) }
+    it { should allow_value('http://google.com').for(:url) }
 
+    it { should allow_value('5.1.1').for(:platform_version) }
+    it { should allow_value('5.0').for(:platform_version) }
+    it { should_not allow_value('5').for(:platform_version) }
+    it { should allow_value('1.1.1').for(:app_version) }
+    it { should allow_value('1.0').for(:app_version) }
+    it { should_not allow_value('1').for(:app_version) }
+    it { should ensure_inclusion_of(:platform).in_array(%w(android ios)) }
   end
 
   describe 'for_client' do
-    mobile_message = FactoryGirl.create(:mobile_message)
-    message = MobileMessage.for_client({:platform => mobile_message.platform,
-                                        :platform_version => mobile_message.platform_version,
-                                        :app_version => mobile_message.app_version}).last
+
+    before :each do
+      @mobile_message = FactoryGirl.create(:mobile_message)
+      @message = MobileMessage.for_client({:platform => @mobile_message.platform,
+                                          :platform_version => @mobile_message.platform_version,
+                                          :app_version => @mobile_message.app_version}).last
+    end
 
     it 'check platform' do
-      message.platform.should == 'ios'
+      @mobile_message.platform.should == 'ios'
     end
 
     it 'check platform_version' do
-      message.platform_version.should == mobile_message.platform_version
+      @mobile_message.platform_version.should == @mobile_message.platform_version
     end
 
     it 'check app_version' do
-      message.app_version.should == mobile_message.app_version
+      @mobile_message.app_version.should == @mobile_message.app_version
     end
 
     it 'check message_type' do
-      message.message_type.should == mobile_message.message_type
+      @mobile_message.message_type.should == @mobile_message.message_type
     end
 
     it 'check url' do
-      message.url.should == mobile_message.url
+      @mobile_message.url.should == @mobile_message.url
     end
 
     it 'check dismissible' do
-      message.dismissible.should == mobile_message.dismissible
+      @mobile_message.dismissible.should == @mobile_message.dismissible
     end
 
     it 'check active' do
-      message.active.should == mobile_message.active
+      @mobile_message.active.should == @mobile_message.active
     end
-
-    message.destroy
 
   end
 
